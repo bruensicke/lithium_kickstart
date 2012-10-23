59efe7cfc6f9ee60ec12241c0e6a28fed7e837c3
d1rk <d1rk@gmx.de>
2012-10-23 22:18:33 +0200

    trim version and add meta infos for App

--- config/app.php
+++ config/app.php
@@ -2,6 +2,9 @@
 return array(
 	'App' => array(
 		'name' => 'lithium_kickstart',
-		'version' => file_get_contents(LITHIUM_APP_PATH.'/VERSION.txt'),
+		'version' => trim(file_get_contents(LITHIUM_APP_PATH.'/VERSION.txt')),
+		'author' => '',
+		'keywords' => '',
+		'description' => '',
 	),
 );
