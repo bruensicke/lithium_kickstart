670bd3d1217fe82d07a7b5c631e4683ceb8f1514
d1rk <d1rk@gmx.de>
2012-10-31 22:09:12 +0100

    update layouts with js in bottom element

--- views/elements/bottom.html.php
+++ views/elements/bottom.html.php
@@ -1,1 +1,26 @@
-<?php /* best place to put analytics code and other stuff */ ?>
+<script type="text/javascript" charset="utf-8">
+head.js(
+	"<?php echo $this->url('/js/jquery.min.js'); ?>",
+	"<?php echo $this->url('/js/icanhaz.min.js'); ?>",
+	"<?php echo $this->url('/js/bootstrap.min.js'); ?>",
+	"<?php echo $this->url('/js/bootbox.min.js'); ?>",
+	"<?php echo $this->url('/js/moment.min.js'); ?>",
+	"<?php echo $this->url('/js/tools.js'); ?>",
+	"<?php echo $this->url('/js/app.js'); ?>",
+	function() {
+		ich.grabTemplates();
+	}
+);
+</script>
+<?php /*
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-XXXXX-Y']);
+_gaq.push(['_trackPageview']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+*/ ?>
--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html class="<?php echo \lithium\core\Environment::get(); ?>">
+<html class="<?= \lithium\core\Environment::get(); ?>">
 <head>
-	<?php echo $this->_render('element', 'head'); ?>
+	<?= $this->_render('element', 'head'); ?>
 </head>
 <body class="app" data-spy="scroll">
-	<?php echo $this->_render('element', 'topnav'); ?>
+	<?= $this->_render('element', 'topnav'); ?>
 	<div class="container">
 		<header id="header">
 			<?= $this->_render('element', 'header'); ?>
@@ -17,20 +17,6 @@
 			<?= $this->_render('element', 'footer'); ?>
 		</footer>
 	</div>
-	<script type="text/javascript" charset="utf-8">
-		head.js(
-			"<?php echo $this->url('/js/jquery.min.js'); ?>",
-			"<?php echo $this->url('/js/icanhaz.min.js'); ?>",
-			"<?php echo $this->url('/js/bootstrap.min.js'); ?>",
-			"<?php echo $this->url('/js/bootbox.min.js'); ?>",
-			"<?php echo $this->url('/js/moment.min.js'); ?>",
-			"<?php echo $this->url('/js/tools.js'); ?>",
-			"<?php echo $this->url('/js/app.js'); ?>",
-			function() {
-				ich.grabTemplates();
-			}
-		);
-	</script>
 	<?= $this->_render('element', 'bottom'); ?>
 </body>
 </html>
